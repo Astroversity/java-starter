1917e0033968c69810c8836be8d9d23006b9f1d1
Astroversity <new2inspire@gmail.com>
2021-11-16 16:21:47 -0600

    If for one langauge = English

--- src/main/java/com/cruftbusters/java_starter/App.java
+++ src/main/java/com/cruftbusters/java_starter/App.java
@@ -1,11 +1,18 @@
 package com.cruftbusters.java_starter;
 
+import java.util.Objects;
+
 public class App {
+  private String language;
+  public App(String language){this.language=language;}
   public String getGreeting(String name) {
-    return String.format("What's up, %s?", name);
+    if (Objects.equals(language, "English")){
+      return String.format("What's up, %s?", name);
+    }
+    return String.format("Hola, %s?", name);
   }
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting("Dan"));
+    System.out.println(new App("English").getGreeting("Dan"));
   }
 }
--- src/test/java/com/cruftbusters/java_starter/AppTest.java
+++ src/test/java/com/cruftbusters/java_starter/AppTest.java
@@ -4,10 +4,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  @Test void appHasAGreeting() {
-    App greeter = new App();
+  @Test void appHasAGreetingInEnglish() {
+    App greeter = new App("English");
     assertEquals("What's up, Dan?", greeter.getGreeting("Dan"));
     assertEquals("What's up, Smith?", greeter.getGreeting("Smith"));
+
+  }
+
+  @Test void appHasAGreetingInSpanish() {
+    App greeter = new App("Spanish");
+    assertEquals("Hola, Dan?", greeter.getGreeting("Dan"));
+    assertEquals("Hola, Smith?", greeter.getGreeting("Smith"));
 
   }
 }
