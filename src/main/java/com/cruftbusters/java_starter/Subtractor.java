233d4e637409d1f9281c0942713faba83df8a123
Astroversity <new2inspire@gmail.com>
2021-11-17 17:10:29 -0600

    some enlightened refactor

--- src/main/java/com/cruftbusters/java_starter/Subtractor.java
+++ src/main/java/com/cruftbusters/java_starter/Subtractor.java
@@ -3,8 +3,10 @@ package com.cruftbusters.java_starter;
 import java.util.List;
 
 public class Subtractor {
-    public List<String> subtract(List<String> someList, String StringToRemove) {
-        someList.remove(StringToRemove);
+    public List<String> subtract(List<String> someList, List<String> stringsToRemove) {
+        for (String stringToRemove : stringsToRemove) {
+          someList.remove(stringToRemove);
+        }
         return someList;
     }
 }
--- src/test/java/com/cruftbusters/java_starter/CollectionTest.java
+++ src/test/java/com/cruftbusters/java_starter/CollectionTest.java
@@ -11,12 +11,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CollectionTest {
     @Test
     void subrtractList() {
+        List<String> stringToRemove = new ArrayList<>(){{
+            add("Make something up");
+        }};
         Subtractor subtractor = new Subtractor();
-        List<String> someList = new ArrayList<>();
-        someList.add("Put another string in there");
-        someList.add("Make something up");
-        List<String> expected = new ArrayList<>();
-        expected.add("Put another string in there");
-        assertEquals(expected, subtractor.subtract(someList, "Make something up"));
+        List<String> someList = new ArrayList<>(){{
+            add("Put another string in there");
+            add("Make something up");
+        }};
+        List<String> expected = new ArrayList<>(){{
+            add("Put another string in there");
+        }};
+        assertEquals(expected, subtractor.subtract(someList, stringToRemove));
+    }
+    @Test
+    void anotherList() {
+        List<String> stringsToRemove = new ArrayList<>() {{
+            add("Make something up");
+            add("Put another string in there");
+        }};
+        Subtractor subtractor = new Subtractor();
+        List<String> someList = new ArrayList<>() {{
+            add("Put another string in there");
+            add("Make something up");
+            add("Here's another line");
+        }};
+        List<String> expected = new ArrayList<>() {{
+            add("Here's another line");
+        }};
+        assertEquals(expected, subtractor.subtract(someList, stringsToRemove));
     }
 }
