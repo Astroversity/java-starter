7e7be601e341a0e5131d2a6dc66e6549a1e3c4f9
Astroversity <new2inspire@gmail.com>
2021-12-07 16:45:25 -0600

    CalcParser with 2 deep

--- src/main/java/com/cruftbusters/java_starter/CalculationParser.java
+++ src/main/java/com/cruftbusters/java_starter/CalculationParser.java
@@ -4,20 +4,37 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import java.util.List;
+import java.util.Objects;
 
 public class CalculationParser {
     BiNode parse(List<String> tokens) {
-        return new BiNode() {{
-            left = new BiNode() {{
-                token = tokens.get(0);
-            }};
-            token = tokens.get(1);
-            right = new BiNode() {{
-                token = tokens.get(2);
-            }};
-        }};
+        BiNode root = null;
+        for (String token : tokens) {
+            if (root == null) {
+                root = new BiNode();
+                root.token = token;
+            } else if (Objects.equals(token, "*")) {
+                BiNode newRight = new BiNode();
+                newRight.token = token;
+                newRight.left = root.right;
+                root.right = newRight;
+            } else if (Objects.equals(token, "+")) {
+                BiNode newRoot = new BiNode();
+                newRoot.left = root;
+                newRoot.token = token;
+                root = newRoot;
+            } else if (root.right == null ){
+                root.right = new BiNode();
+                root.right.token = token;
+            } else {
+                root.right.right = new BiNode();
+                root.right.right.token = token;
+            }
+        }
+        return root;
     }
 }
+
 @EqualsAndHashCode
 @ToString
 class BiNode {
--- src/test/java/com/cruftbusters/java_starter/CalculationParserTest.java
+++ src/test/java/com/cruftbusters/java_starter/CalculationParserTest.java
@@ -9,7 +9,7 @@ class CalculationParserTest {
     @Test
     void parseOnePlusOne() {
         new CalculationParser() {{
-            BiNode biNode = parse(Arrays.asList("1","+","1"));
+            BiNode biNode = parse(Arrays.asList("1", "+", "1"));
             Assertions.assertEquals(
                     new BiNode() {{
                         left = new BiNode() {{
@@ -18,6 +18,31 @@ class CalculationParserTest {
                         token = "+";
                         right = new BiNode() {{
                             token = "1";
+                        }};
+                    }},
+                    biNode
+            );
+        }};
+    }
+
+    @Test
+    void parseOnePlusOneTimesTwo() {
+        new CalculationParser() {{
+            BiNode biNode = parse(Arrays.asList("1", "+", "1", "*", "2"));
+            Assertions.assertEquals(
+                    new BiNode() {{
+                        left = new BiNode() {{
+                            token = "1";
+                        }};
+                        token = "+";
+                        right = new BiNode() {{
+                            left = new BiNode() {{
+                                token = "1";
+                            }};
+                            token = "*";
+                            right = new BiNode() {{
+                                token = "2";
+                            }};
                         }};
                     }},
                     biNode
