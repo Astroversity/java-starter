95af81970a6dcf436df393df5564f1cb46ecafe1
Astroversity <new2inspire@gmail.com>
2021-12-08 16:19:09 -0600

    InputCalc removed second result method

--- src/main/java/com/cruftbusters/java_starter/InputCalc.java
+++ src/main/java/com/cruftbusters/java_starter/InputCalc.java
@@ -1,7 +1,5 @@
 package com.cruftbusters.java_starter;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class InputCalc {
@@ -20,37 +18,5 @@ public class InputCalc {
 
     private boolean isOperator(String token) {
         return Objects.equals(token, "*") || Objects.equals(token, "+");
-    }
-
-    int result(String userInput) {
-        List<Integer> numbersToMath = new ArrayList<>();
-        int result = 0;
-        String[] userInputArray = userInput.split("[+\\-/*]");
-        for (String splitString : userInputArray) {
-            int number = Integer.parseInt(splitString.trim());
-            numbersToMath.add(number);
-        }
-        for (int i = 0; i < numbersToMath.size(); i++) {
-            if (userInput.contains("+")) {
-                result += numbersToMath.get(i);
-            } else if (userInput.contains("-")) {
-                if (i == 0) {
-                    result = numbersToMath.get(0);
-                } else result -= numbersToMath.get(i);
-            } else if (userInput.contains("*")) {
-                if (i == 0) {
-                    result = numbersToMath.get(0);
-                } else result *= numbersToMath.get(i);
-            } else if (userInput.contains("/")) {
-                if (i == 0) {
-                    result = numbersToMath.get(0);
-                } else try {
-                    result /= numbersToMath.get(i);
-                } catch (ArithmeticException e) {
-                    throw new ArithmeticException("Can't be divided by 0");
-                }
-            }
-        }
-        return result;
     }
 }
--- src/test/java/com/cruftbusters/java_starter/InputCalcTest.java
+++ src/test/java/com/cruftbusters/java_starter/InputCalcTest.java
@@ -5,15 +5,6 @@ import org.junit.jupiter.api.Test;
 
 class InputCalcTest {
     @Test
-    void Addition() {
-        new InputCalc() {{
-            Assertions.assertEquals(2, result("1+1"));
-            Assertions.assertEquals(4, result("2+2"));
-            Assertions.assertEquals(13, result("4+9"));
-        }};
-    }
-
-    @Test
     void AdditionWithBiNode() {
         new InputCalc() {{
             Assertions.assertEquals(2, result(
@@ -29,6 +20,7 @@ class InputCalcTest {
             ));
         }};
     }
+
     @Test
     void TimesWithBiNode() {
         new InputCalc() {{
@@ -68,6 +60,7 @@ class InputCalcTest {
             ));
         }};
     }
+
     @Test
     void TwoSubTrees() {
         new InputCalc() {{
@@ -96,6 +89,7 @@ class InputCalcTest {
             ));
         }};
     }
+
     @Test
     void PlusAndTimesRightSubTree() {
         new InputCalc() {{
@@ -118,6 +112,7 @@ class InputCalcTest {
             ));
         }};
     }
+
     @Test
     void PlusAndPlusRightSubTree() {
         new InputCalc() {{
@@ -138,62 +133,6 @@ class InputCalcTest {
                         }};
                     }}
             ));
-        }};
-    }
-
-    @Test
-    void Subtraction() {
-        new InputCalc() {{
-            Assertions.assertEquals(4, result("8-4"));
-            Assertions.assertEquals(1, result("6-5"));
-            Assertions.assertEquals(3, result("3-0"));
-        }};
-    }
-
-    @Test
-    void Multiply() {
-        new InputCalc() {{
-            Assertions.assertEquals(6, result("3*2"));
-            Assertions.assertEquals(4, result("2*2"));
-            Assertions.assertEquals(63, result("7*9"));
-        }};
-    }
-
-    @Test
-    void Divide() throws ArithmeticException {
-        new InputCalc() {{
-            Assertions.assertEquals(4, result("8/2"));
-            Assertions.assertEquals(2, result("8/4"));
-            ArithmeticException exception = Assertions.assertThrows(
-                    ArithmeticException.class,
-                    () -> result("8/0"));
-            Assertions.assertEquals("Can't be divided by 0", exception.getMessage());
-        }};
-    }
-
-    @Test
-    void InputWithSpaces() {
-        new InputCalc() {{
-            Assertions.assertEquals(14, result("5 + 9"));
-            Assertions.assertEquals(7, result(" 4 + 3 "));
-        }};
-    }
-
-    @Test
-    void DoubleDigits() {
-        new InputCalc() {{
-            Assertions.assertEquals(68, result("23 + 45"));
-            Assertions.assertEquals(1035, result("23 * 45"));
-            Assertions.assertEquals(4, result("48 / 12"));
-        }};
-    }
-
-    @Test
-    void TripleDigits() {
-        new InputCalc() {{
-            Assertions.assertEquals(1300, result("721 + 123 + 456"));
-            Assertions.assertEquals(142, result("721 - 123 - 456"));
-            Assertions.assertEquals(40439448, result("721 * 123 * 456"));
         }};
     }
 }
