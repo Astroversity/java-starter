7a4673bdeabdb46b148a0032eccecc8ef20ffe3b
tyler <tyler@cruftbusters.com>
2022-01-26 02:55:34 -0600

    use withEmbeddedServer to enable concurrent execution of embedded server tests

--- src/test/java/com/cruftbusters/java_starter/SolverControllerKtTest.kt
+++ src/test/java/com/cruftbusters/java_starter/SolverControllerKtTest.kt
@@ -17,49 +17,45 @@ import org.slf4j.Logger
 import java.lang.RuntimeException
 
 class SolverControllerKtTest : FunSpec({
-    val mockSolver = mockk<Solver>()
-    val mockLogger = mockk<Logger>(relaxUnitFun = true)
-    val mockValidator = mockk<Validator>(relaxed = true)
-    embeddedServer(Netty, port = 8079) {
-        solverController(
-            mockValidator,
-            mockSolver,
-            mockLogger,
-        )
-    }.start()
-    val httpClient = HttpClient {
-        defaultRequest {
-            url.protocol = URLProtocol.HTTP
-            url.host = "localhost"
-            url.port = 8079
-        }
-    }
-    test("Valid Equation") {
-        every { mockSolver.solve("1+1") } returns 2
-        httpClient.get<String>("/") {
-            parameter("equation", "1+1")
-        } shouldBe "2"
-    }
-    test("Log Error") {
-        val error = RuntimeException("oopsy whoopsy")
-        every { mockSolver.solve("6*9") } throws error
-        val someException = shouldThrow<ServerResponseException> {
-            httpClient.get<Unit>("/") {
-                parameter("equation", "6*9")
+    context("with embedded server") {
+        val mockSolver = mockk<Solver>()
+        val mockLogger = mockk<Logger>(relaxUnitFun = true)
+        val mockValidator = mockk<Validator>(relaxed = true)
+        withEmbeddedServer({
+            solverController(
+                mockValidator,
+                mockSolver,
+                mockLogger,
+            )
+        }) { httpClient ->
+            test("Valid Equation") {
+                every { mockSolver.solve("1+1") } returns 2
+                httpClient.get<String>("/") {
+                    parameter("equation", "1+1")
+                } shouldBe "2"
+            }
+            test("Log Error") {
+                val error = RuntimeException("oopsy whoopsy")
+                every { mockSolver.solve("6*9") } throws error
+                val someException = shouldThrow<ServerResponseException> {
+                    httpClient.get<Unit>("/") {
+                        parameter("equation", "6*9")
+                    }
+                }
+                someException.response.status.shouldBe(HttpStatusCode.InternalServerError)
+                verify { mockLogger.error("There was an error while solving an equation", error) }
+            }
+            test("Describe Invalid Equation") {
+                val error = "Invalid Parenthesis"
+                every { mockValidator.validate("(1+7") } returns error
+                val someException = shouldThrow<ClientRequestException> {
+                    httpClient.get<Unit>("/") {
+                        parameter("equation", "(1+7")
+                    }
+                }
+                someException.response.status.shouldBe(HttpStatusCode.BadRequest)
+                someException.response.receive<String>() shouldBe error
             }
         }
-        someException.response.status.shouldBe(HttpStatusCode.InternalServerError)
-        verify { mockLogger.error("There was an error while solving an equation", error) }
-    }
-    test("Describe Invalid Equation") {
-        val error = "Invalid Parenthesis"
-        every { mockValidator.validate("(1+7") } returns error
-        val someException = shouldThrow<ClientRequestException> {
-            httpClient.get<Unit>("/") {
-                parameter("equation", "(1+7")
-            }
-        }
-        someException.response.status.shouldBe(HttpStatusCode.BadRequest)
-        someException.response.receive<String>() shouldBe error
     }
 })
